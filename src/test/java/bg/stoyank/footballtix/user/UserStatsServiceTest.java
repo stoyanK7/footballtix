b6ca489253523245e5fcff046f00b50e8b4b841a
stoyan <s.kostadinov@student.fontys.nl>
2021-12-02 12:24:16 +0100

    Removed docs folder

---
+++ src/test/java/bg/stoyank/footballtix/user/UserStatsServiceTest.java
@@ -0,0 +1,19 @@
+package bg.stoyank.footballtix.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserStatsServiceTest {
+
+    @Test
+    void getAllByDateBetween() {
+    }
+
+    @Test
+    void createNewDayStat() {
+    }
+
+    @Test
+    void increaseCountByOne() {
+    }
+}
